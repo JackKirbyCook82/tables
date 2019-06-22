5ac5b97a1d0414f415ceef74ce1cef029f1f41b5
JackKirbyCook82 <jackcookchee@yahoo.com>
2019-06-22 17:46:20 -0500

    Updated

--- __init__.py
+++ __init__.py
@@ -12,6 +12,9 @@ import numpy as np
 import json
 from functools import update_wrapper
 
+from utilities.dataframes import dataframe_fromxarray
+from utilities.arrays import xarray_fromdataframe
+
 from tables.operations import OPERATIONS
 
 __version__ = "1.0.0"
@@ -38,26 +41,34 @@ def get_option(key): return _OPTIONS[key]
 def show_options(): print('Table Options: ' + ', '.join([' = '.join([key, str(value)]) for key, value in _OPTIONS.items()]))
 
 
-_buffer = _OPTIONS['bufferchar'] * _OPTIONS['linewidth']
+_buffer = lambda : _OPTIONS['bufferchar'] * _OPTIONS['linewidth']
 
 
 class TableBase(ABC):
-    def __init__(self, *args, data, name='table', variables, **kwargs): 
-        self.__tabledata = data
-        self.__tablename = name
+    def __init__(self, data, key, name, variables, **kwargs): 
+        self.__data = data
+        self.__key = key
+        self.__name = name
         self.__variables = variables
     
     @property
-    def name(self): return self.__tablename
+    def data(self): return self.__data
     @property
-    def __name(self): return ' '.join([self.__tablename.upper(), 'as', self.__class__.__name__.upper()])
+    def key(self): return self.__key   
     @property
-    def data(self): return self.__tabledata
+    def name(self): return self.__name
     @property
     def variables(self): return self.__variables
     
-    def __str__(self, *args, **kwargs): return '\n'.join([_buffer, '\n\n'.join([self.__name, *self.strings, 'Dim={}, Shape={}'.format(self.dim, self.shape)]), _buffer])        
+    @property
+    def tablename(self): return ' '.join([self.__name.upper(), 'as', self.__class__.__name__.upper()])    
+    def todict(self): return dict(data=self.data, key=self.key, name=self.name, variables=self.variables)
+    
+    def __str__(self, *args, **kwargs): return '\n'.join([_buffer(), '\n\n'.join([self.tablename, *self.strings, *self.varstrings, 'Dim={}, Shape={}'.format(self.dim, self.shape)]), _buffer()])        
     def __len__(self): return self.dim    
+    
+    @property
+    def varstrings(self): return ['VARIABLES' + '\n' + str(self.variables)]
     
     @abstractmethod
     def strings(self): pass
@@ -68,27 +79,22 @@ class TableBase(ABC):
  
 
 class ArrayTable(TableBase):
-    def __init__(self, xarray, *args, **kwargs):
-        self.__xarray = xarray
-        super().__init__(*args, **kwargs)
-    
     @property
-    def xarray(self): return self.__xarray   
-    def todict(self): return dict(xarray=self.xarray, data=self.data, variables=self.variables)
+    def xarray(self): return self.data   
     
     @property
     def dim(self): return len(self.xarray.dims)
     @property
     def shape(self): return self.xarray.shape 
     
-    def axiskey(self, axis): return self.__xarray.dim[axis] if isinstance(axis, int) else axis
-    def axisindex(self, axis): return self.__xarray.get_axis_num(axis) if isinstance(axis, str) else axis
+    def axiskey(self, axis): return self.xarray.dim[axis] if isinstance(axis, int) else axis
+    def axisindex(self, axis): return self.xarray.get_axis_num(axis) if isinstance(axis, str) else axis
     
     @property
     def strings(self):
-        data = self.__xarray.values
-        headers = json.dumps({dim:list(self.__xarray.coords[dim].values) for dim in self.__xarray.dims}, sort_keys=False, indent=3, separators=(',', ' : '))
-        scope = json.dumps(self.__xarray.attrs, sort_keys=False, indent=3, separators=(',', ' : '))
+        data = self.xarray.values
+        headers = json.dumps({dim:list(self.xarray.coords[dim].values) for dim in self.xarray.dims}, sort_keys=False, indent=3, separators=(',', ' : '))
+        scope = json.dumps(self.xarray.attrs, sort_keys=False, indent=3, separators=(',', ' : '))
         return ['DATA\n' + str(data), 'HEADERS\n' + str(headers), 'SCOPE\n' + str(scope)]    
 
     def __add__(self, other): return self.add(other)
@@ -103,32 +109,35 @@ class ArrayTable(TableBase):
         def wrapper(other, *args, **kwargs): return operation_function(self, other, *args, **kwargs)
         update_wrapper(wrapper, operation_function)   
         return wrapper
-        
+    
+    def __getitem__(self, key): 
+        return self.__class__(self.xarray[key], key=self.key, name=self.name, variables=self.variables)
+    
     def flatten(self): 
-        pass        
+        return FlatTable(dataframe_fromxarray(self.xarray, self.key), key=self.key, name=self.name, variables=self.variables)     
 
 
 class FlatTable(TableBase):
-    def __init__(self, dataframe, *args, **kwargs):
-        self.__dataframe = dataframe
-        super().__init__(*args, **kwargs)
+    @property
+    def dataframe(self): return self.data
 
     @property
-    def dataframe(self): return self.__dataframe
+    def dim(self): return len(self.headercolumns)
     @property
-    def variables(self): return self.__variables
+    def shape(self): return tuple([len(set(self.dataframe[column].values)) for column in self.headercolumns])
 
     @property
-    def dim(self): pass
-    @property
-    def shape(self): pass
+    def strings(self): return ['DATA' + '\n' + str(self.dataframe)]
 
     @property
-    def strings(self):
-        pass
-
+    def datacolumns(self): return [self.key]
+    @property
+    def scopecolumns(self): return [column for column in self.dataframe.columns if len(set(self.dataframe[column].values)) == 1]
+    @property
+    def headercolumns(self): return [column for column in self.dataframe.columns if all([column not in self.scopecolumns, column != self.key])]
+    
     def unflatten(self):
-        pass
+        return ArrayTable(xarray_fromdataframe(self.dataframe, self.key), key=self.key, name=self.name, variables=self.variables)
     
     
     
--- transformations.py
+++ transformations.py
@@ -14,7 +14,7 @@ import variables.arrays as var
 
 __version__ = "1.0.0"
 __author__ = "Jack Kirby Cook"
-__all__ = ['Normalize', 'Standardize', 'MinMax', 'Average', 'Cumulate', 'Consolidate', 'Interpolate', 'Inversion']
+__all__ = ['Scale', 'Average', 'Cumulate', 'Consolidate', 'Interpolate', 'Inversion']
 __copyright__ = "Copyright 2018, Jack Kirby Cook"
 __license__ = ""
 
@@ -27,105 +27,102 @@ def setheader(xarray, header, axis):
     return xarray 
 
 def tovarray(header, variable): return [variable.fromstr(item) for item in header]
+def tovalues(varray): return [item.value for item in varray]
 def toheader(varray): return [str(item) for item in varray]
 
 
 class Transformation(ABC):
-    def __init__(self, *args, **hyperparms): self.hyperparms.update(hyperparms)               
+    def __init__(self, *args, **hyperparms): 
+        self.hyperparms = {key:value for key, value in self.default_hyperparms.items()}
+        self.hyperparms.update(hyperparms)           
+    def __repr__(self): return '{}({})'.format(self.__class__.__name__, self.hyperparms)
     
     def __call__(self, table, *args, axis, **kwargs):
         TableClass = table.__class__
-        xarray = self.execute(*args, **table.todict(), axis=axis, **kwargs)
-        variables = self.variables(*args, **table.todict(), axis=axis, **kwargs)
-        data = table.data
-        return TableClass(xarray, variables=variables, data=data, name=table.name)
+        content = table.todict()
+        transform = self.execute(*args, **content, axis=axis, **self.hyperparms, **kwargs)
+        content.update(transform)
+        return TableClass(**content)
         
     @abstractmethod
-    def execute(self, *args, xarray, variables, axis, **kwargs): pass
-    @abstractmethod
-    def variables(self, *args, variables, data, axis, **kwargs): pass
+    def execute(self, *args, data, variables, axis, **kwargs): pass
 
-    def update_xarray(self, xarray, *args, axis, **kwargs):
-        return arr.apply_toxarray(xarray, self.functions['xarray'], *args, axis=axis, **self.hyperparms, **kwargs)
-    def update_varray(self, varray, *args, **kwargs):
-        return var.apply_tovarray(varray, self.functions['varray'], *args, **self.hyperparms, **kwargs)
-
-    def update_datavariable(self, variables, *args, data, **kwargs):
-        variables.update({data:getattr(variables[data], self.methods['data'])(*args, **kwargs)})
-        return variables
-    def update_axisvariable(self, variables, *args, axis, **kwargs):
-        variables.update({axis:getattr(variables[axis], self.methods['axis'])(*args, **kwargs)})
-        return variables
+    def update_xarray(self, xarray, *args, axis, **kwargs): return arr.apply_toxarray(xarray, self.functions['xarray'], *args, axis=axis, **kwargs)
+    def update_varray(self, varray, *args, **kwargs): return var.apply_tovarray(varray, self.functions['varray'], *args, **kwargs)
 
     @classmethod
-    def register(cls, xarray=None, varray=None, data=None, axis=None, **hyperparms):  
+    def register(cls, functions, **hyperparms):  
+        assert isinstance(functions, dict)
         def wrapper(subclass):
             name = subclass.__name__
             bases = (subclass, cls)
-            functions, methods = {}, {}
-            if xarray: functions['xarray'] = xarray
-            if varray: functions['varray'] = varray
-            if data is not None: methods['data'] = data
-            if axis is not None: methods['axis'] = axis
-            attrs = dict(hyperparms=hyperparms, functions=functions, methods=methods)           
+            attrs = dict(default_hyperparms=hyperparms, functions=functions)           
             return type(name, bases, attrs)
         return wrapper  
 
 
-@Transformation.register(xarray=arr.normalize, data='normalized')
-class Normalize: 
-    def execute(self, *args, xarray, variables, axis, **kwargs):
-        xarray = self.update_xarray(xarray, *args, axis=axis, **kwargs)
-        return xarray
-    
-    def variables(self, *args, variables, data, axis, **kwargs):
-        variables = self.update_datavariable(variables, *args, data=data, **kwargs)
-        return variables
-        
-    
-@Transformation.register(xarray=arr.standardize, data='standardized')
-class Standardize: 
-    pass
+@Transformation.register({'xarray':arr.scale})
+class Scale: 
+    def execute(self, *args, data, variables, key, axis, **kwargs):
+        xarray = self.update_xarray(data, *args, axis=axis, **kwargs)
+        variables[key] = getattr(variables[key], 'scale')(*args, **kwargs)
+        return {'data':xarray, 'variables':variables}
 
 
-@Transformation.register(xarray=arr.minmax, data='minmaxed')
-class MinMax: 
-    pass
-
-
-@Transformation.register(xarray=arr.average, varray=var.summation, data='averaged', weights=None)
+@Transformation.register({'xarray':arr.average, 'varray':var.summation}, weights=None)
 class Average: 
-    pass
-    
-    
-@Transformation.register(xarray=arr.cumulate, varray=var.cumulate, direction='upper')
-class Cumulate: 
-    def execute(self, *args, xarray, variables, axis, **kwargs):
-        header = getheader(xarray, axis)
+    def execute(self, *args, data, variables, key, axis, **kwargs):
+        header = getheader(data, axis)
         varray = tovarray(header, variables[axis])
-        xarray = self.update_xarray(xarray, *args, axis=axis, **kwargs)
-        varray = self.update_varray(varray, *args, axis=axis, **kwargs)
+        xarray = self.update_xarray(data, *args, axis=axis, **kwargs)
+        varray = self.update_varray(varray, *args, **kwargs)
         header = toheader(varray)
         xarray = setheader(xarray, header, axis)
-        return xarray
+        variables[key] = getattr(variables[key], 'modify')(*args, mod='average', **kwargs)
+        return {'data':xarray, 'variables':variables}
     
-    def variables(self, *args, variables, data, axis, **kwargs): 
-        return variables
+    
+@Transformation.register({'xarray':arr.cumulate, 'varray':var.cumulate}, direction='lower')
+class Cumulate: 
+    def execute(self, *args, data, variables, axis, **kwargs):
+        header = getheader(data, axis)
+        varray = tovarray(header, variables[axis])
+        xarray = self.update_xarray(data, *args, axis=axis, **kwargs)
+        varray = self.update_varray(varray, *args, **kwargs)
+        header = toheader(varray)
+        xarray = setheader(xarray, header, axis)
+        return {'data':xarray}
       
         
-@Transformation.register(varray=var.consolidate, method='average')
+@Transformation.register({'varray':var.consolidate})
 class Consolidate: 
-    pass
+    def execute(self, *args, data, variables, key, axis, **kwargs):
+        header = getheader(data, axis)
+        varray = tovarray(header, variables[axis])
+        varray = self.update_varray(varray, *args, **kwargs)
+        header = toheader(varray)
+        xarray = setheader(data, header, axis)
+        variables[axis] = getattr(variables[axis], 'consolidate')(*args, **kwargs)
+        return {'data': xarray, 'variables':variables}
     
     
-@Transformation.register(xarray=arr.interpolate1D, kind='linear', fill='extrapolate')
+@Transformation.register({'xarray':arr.interpolate1D, 'varray':var.varray_fromvalues}, kind='linear', fill='extrapolate')
 class Interpolate: 
-    pass
+    def execute(self, *args, data, variables, key, axis, values, **kwargs):
+        header = getheader(data, axis)
+        varray = tovarray(header, variables[axis])
+        hdrvalues = tovalues(varray)
+        xarray = self.update_xarray(data, hdrvalues, values, *args, axis=axis, **kwargs)
+        varray = self.update_varray(varray, *args, variables[axis], **kwargs)
+        header = toheader(varray)
+        xarray = setheader(xarray, header, axis)
+        return {'data':xarray}
     
     
-@Transformation.register(xarray=arr.interpolate1D, kind='linear', fill='extrapolate')
+@Transformation.register({'xarray':arr.interpolate1D, 'varray':var.varray_fromvalues}, kind='linear', fill='extrapolate')
 class Inversion: 
-    pass
+    def execute(self, *args, data, variables, key, axis, values, **kwargs):
+        pass
 
 
 
