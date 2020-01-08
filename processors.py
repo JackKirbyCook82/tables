846081eedc40025821645cccec4a0b7ef49a8a07
JackKirbyCook82 <jackcookchee@yahoo.com>
2020-01-08 14:20:46 -0800

    Updated

--- processors.py
+++ processors.py
@@ -23,9 +23,7 @@ _aslist = lambda items: [items] if not isinstance(items, (list, tuple)) else lis
 
 MetaSgmts = ntuple('MetaSgmts', 'universe headers scope')
 class Meta(MetaSgmts):
-    def todict(self): return self._asdict()   
-    def __new__(cls, universe, *headers, **scope): return super().__new__(cls, universe, headers, scope)
-         
+    def __new__(cls, universe, *headers, **scope): return super().__new__(cls, universe, headers, scope)        
     def __str__(self): 
         universe = self.universe
         headers = (self.headers[0] if len(self.headers) == 1 else ', '.join(list(self.headers))) if self.headers else None
@@ -35,10 +33,9 @@ class Meta(MetaSgmts):
 
 
 class Pipeline(Node):
-    def __init__(self, key, function, tables, parms, meta, parent=None, children=[]):
+    def __init__(self, key, function, parms, meta, parent=None, children=[]):
         assert isinstance(parms, dict)
         self.__function = function
-        self.__tables = _aslist(tables)
         self.__parms = parms
         self.__meta = meta
         self.__table = None
@@ -49,15 +46,13 @@ class Pipeline(Node):
     @property
     def table(self): return self.__table
     @property
-    def tables(self): return self.__tables
-    @property
     def meta(self): return self.__meta
     @property
     def parms(self): return self.__parms
 
     def __str__(self): 
         content = {}
-        if self.__tables: content['tables'] = self.__tables
+        if self.children: content['tables'] = [str(child.key) for child in self.children]
         if self.__parms: content['parms'] = self.__parms
         if self.__meta: content['meta'] = json.loads(str(self.__meta))
         return json.dumps(content, sort_keys=False, indent=3, separators=(',', ' : '), default=str)  
@@ -73,35 +68,77 @@ class Pipeline(Node):
 
 class FrozenCalculationError(Exception): pass
 
+def freeze(function):
+    def wrapper(self, *args, **kwargs):
+        if self.frozen: raise FrozenCalculationError(repr(self))
+        else: return function(self, *args, **kwargs)
+    return wrapper
+    
 
-class Calculation(Tree):     
+class Calculation(Tree):  
     @property
     def frozen(self): return self.__frozen
+    @property
+    def queue(self): return self.__queue
+    
     def __init__(self, *args, **kwargs):
+        self.__queue = {}
         self.__frozen = False
         super().__init__(*args, **kwargs)
     
     def __str__(self):
         namestr = '{} ("{}")'.format(self.name if self.name else self.__class__.__name__, self.key)
-        content = {key:{**json.loads(str(pipeline.meta)), 'tables':', '.join(pipeline.tables)} if pipeline.tables else json.loads(str(pipeline.meta)) for key, pipeline in self.items()}        
+        content = {key:{**json.loads(str(pipeline.meta)), 'tables':', '.join([str(child.key) for child in pipeline.children])} for key, pipeline in iter(self)}        
         jsonstr = json.dumps(content, sort_keys=False, indent=3, separators=(',', ' : '), default=str)  
         return ' '.join([namestr, jsonstr])
-        
+
     def create(self, **kwargs):
-        if self.frozen: raise FrozenCalculationError(repr(self))
         def decorator(function):
-            create_pipeline = lambda key, values: Pipeline(key, function, tables=values.get('tables', []), parms=values.get('parms', {}), meta=values['meta'])
-            self.update({key:create_pipeline(key, values) for key, values in kwargs.items()})
+            pipelines = [Pipeline(key, function, value.get('parms', {}), value['meta']) for key, value in kwargs.items()]
+            queue = {key:_aslist(value.get('tables', [])) for key, value in kwargs.items()}
+            assert not any([key in self.__queue.keys() for key in queue.keys()])
+            super(Calculation, self).append(*pipelines)
+            self.__queue.update(queue)
             return function
         return decorator
+    
+    def append(self, *args, **kwargs): raise NotImplementedError('{}.{}()'.format(self.__class__.__name__, 'append'))    
+    
+    @freeze
+    def __iadd__(self, other):
+        assert isinstance(other, type(self))   
+        assert not any([otherkey in self.queue.keys() for otherkey in other.queue.keys()])
+        assert not any([otherkey in [nodekey for nodekey, node in iter(self)] for otherkey, other in iter(other)])                
+        self.__queue.update(other.queue)
+        super(Calculation, self).append(*[node for nodekey, node in iter(other)])
+        return self  
 
+    @freeze
     def __call__(self, *args, **kwargs):
-        if self.frozen: raise FrozenCalculationError(repr(self))
-        for key, pipeline in self.items(): 
-            self[key].addchildren(*[self[tablekey] for tablekey in pipeline.tables])
+        for nodekey, childrenkeys in self.queue.items(): 
+            self[nodekey].addchildren(*[self[childkey] for childkey in childrenkeys if self[childkey] not in self[nodekey].children])
+        self.__queue = {}
         self.__frozen = True
+        return
 
-    
-    
 
-    
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
